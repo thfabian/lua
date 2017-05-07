5311ed04dbd28641191eddd9a44eda72fcdb629d
Fabian Thüring <fabian_thuering@hotmail.com>
2017-05-07 14:45:24 +0200

    add install check script

---
+++ test/check-install.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#*****************************************************************************#
+# Copyright (C) 1994-2017 Lua.org, PUC-Rio.
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+#*****************************************************************************#
+
+if [ "$#" -lt 1 ]; then 
+    echo "usage: $0 <install-dir>"
+    exit 1
+fi
+
+install_dir=$1
+
+function lua_check_install {
+  ls $1 | grep -q $2
+  if [ "$?" == "1" ]; then
+    echo "error: pattern '$2' not found in $1"
+    exit 1
+  fi
+}
+
+lua_check_install $install_dir/lib liblua
+lua_check_install $install_dir/include lua.h
+lua_check_install $install_dir/bin luac
+lua_check_install $install_dir/bin lua
